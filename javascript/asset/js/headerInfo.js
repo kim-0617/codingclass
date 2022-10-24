5258d2e6fae5bfb392abc666a9104bc4561c09c3
kim-0617 <kkk5993@naver.com>
2022-10-24 19:19:42 +0900

    2022-10-24

--- javascript/asset/js/headerInfo.js
+++ javascript/asset/js/headerInfo.js
@@ -1,27 +1,27 @@
 const header_template = `
 <ul>
-    <li><a href="/javascript/javascript01.html">데이터 저장하기</a></li>
-    <li><a href="/javascript/javascript02.html">데이터 불러오기</a></li>
-    <li><a href="/javascript/javascript03.html">데이터 실행하기</a></li>
-    <li><a href="/javascript/javascript04.html">데이터 제어하기</a></li>
+    <li><a href="javascript01.html">데이터 저장하기</a></li>
+    <li><a href="javascript02.html">데이터 불러오기</a></li>
+    <li><a href="javascript03.html">데이터 실행하기</a></li>
+    <li><a href="javascript04.html">데이터 제어하기</a></li>
 </ul>
 <ul>
-    <li><a href="/javascript/javascript05.html">문자열 객체</a></li>
-    <li><a href="/javascript/javascript06.html">배열 객체</a></li>
-    <li><a href="/javascript/javascript07.html">수학 객체</a></li>
-    <li><a href="/javascript/javascript08.html">숫자 / 데이트 객체</a></li>
-    <li><a href="/javascript/javascript09.html">브라우저 객체</a></li>
-    <li><a href="/javascript/javascript10.html">요소 객체</a></li>
-    <li><a href="/javascript/javascript11.html">이벤트 객체</a></li>
-    <li><a href="/javascript/javascript12.html">제이쿼리</a></li>
+    <li><a href="javascript05.html">문자열 객체</a></li>
+    <li><a href="javascript06.html">배열 객체</a></li>
+    <li><a href="javascript07.html">수학 객체</a></li>
+    <li><a href="javascript08.html">숫자 / 데이트 객체</a></li>
+    <li><a href="javascript09.html">브라우저 객체</a></li>
+    <li><a href="javascript10.html">요소 객체</a></li>
+    <li><a href="javascript11.html">이벤트 객체</a></li>
+    <li><a href="javascript12.html">제이쿼리</a></li>
 </ul>
 <ul class="effect">
-    <li><a href="/javascript/effect/searchEffect.html">검색 효과</a></li>
-    <li><a href="/javascript/effect/quizEffect.html">퀴즈 효과</a></li>
-    <li><a href="/javascript/effect/mouseEffect.html">마우스 효과</a></li>
-    <li><a href="/javascript/effect/sliderEffect.html">슬라이드 효과</a></li>
-    <li><a href="/javascript/effect/parallaxEffect.html">패랠렉스 효과</a></li>
-    <li><a href="/javascript/effect/gameEffect.html">게임 효과</a></li>
+    <li><a href="effect/searchEffect.html">검색 효과</a></li>
+    <li><a href="effect/quizEffect.html">퀴즈 효과</a></li>
+    <li><a href="effect/mouseEffect.html">마우스 효과</a></li>
+    <li><a href="effect/sliderEffect.html">슬라이드 효과</a></li>
+    <li><a href="effect/parallaxEffect.html">패랠렉스 효과</a></li>
+    <li><a href="effect/gameEffect.html">게임 효과</a></li>
 </ul>
 `;
 
